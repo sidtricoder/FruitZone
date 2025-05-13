6ad830434e743416f75ceb894ca101e3bbd68c3b
sidtricoder <siddharth.tripathi@iiitb.ac.in>
2025-05-13 12:35:15 +0530

    Fixed Bugs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-// import { connectDB } from './config/database'; // We'll create this soon
+import { connectDB } from './config/database'; // Import connectDB
 
 dotenv.config();
 
@@ -13,8 +13,8 @@ app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// TODO: Initialize DB Connection
-// connectDB();
+// Initialize DB Connection
+connectDB(); // Call connectDB here
 
 // Basic Route
 app.get('/', (req: Request, res: Response) => {
