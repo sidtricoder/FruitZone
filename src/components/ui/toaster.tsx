aba014a60f93648c9a270531a977efc02310821b
sidtricoder <siddharth.tripathi@iiitb.ac.in>
2025-05-14 17:03:44 +0530

    Fixed Bugs

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,12 +1,21 @@
+import * as React from "react"
 import { Toast, ToastProvider, ToastViewport } from "./toast"
 import { useToast } from "./use-toast"
+
+interface Toast {
+  id: string;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  action?: React.ReactElement;
+  [key: string]: any;
+}
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({ id, title, description, action, ...props }: Toast) {
         return (
           <Toast key={id} {...props}>
             <div className="grid gap-1">
